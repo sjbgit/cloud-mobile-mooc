cb75eee4f18621adb4927c6b1fa9b0365044abd0
sjbgit <sjb_strat@hotmail.com>
2014-09-21 14:06:22 -0500

    add annotations

--- assignments/Asgn2-VideoLike/src/main/java/org/magnum/mobilecloud/video/Application.java
+++ assignments/Asgn2-VideoLike/src/main/java/org/magnum/mobilecloud/video/Application.java
@@ -4,6 +4,7 @@ import java.io.File;
 
 import org.apache.catalina.connector.Connector;
 import org.apache.coyote.http11.Http11NioProtocol;
+import org.magnum.mobilecloud.video.repository.VideoRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -14,14 +15,20 @@ import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletCon
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import org.magnum.mobilecloud.video.auth.OAuth2SecurityConfiguration;
 
 //Tell Spring to automatically inject any dependencies that are marked in
 //our classes with @Autowired
 @EnableAutoConfiguration
 // Tell Spring to turn on WebMVC (e.g., it should enable the DispatcherServlet
 // so that requests can be routed to our Controllers)
+
+@EnableJpaRepositories(basePackageClasses = VideoRepository.class)
+
 @EnableWebMvc
 // Tell Spring that this object represents a Configuration for the
 // application
